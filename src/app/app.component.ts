de8bef7a2effe9aa67a0f8321a5f917c7d7e3771
Ammar Botonjic <ammar.botonjic@digacon-software.com>
2023-10-29 12:05:53 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  inputText = '';
+  randomSentence = lorem.sentence();
+
+  onInputChange(event: Event) {
+    this.inputText = (event.target as HTMLInputElement).value;
+  }
+
+  getCharacterClass(char: string, index: number): string {
+    if (!this.inputText[index]) {
+      return 'has-text-black';
+    }
+
+    if (char === this.inputText[index]) {
+      return 'has-text-warning';
+    } else {
+      return 'has-text-danger';
+    }
+  }
 }
